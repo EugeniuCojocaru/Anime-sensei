846edeee9898a539f0559e5bf8723b173dd2608e
Eugeniu Cojocaru <69110556+EugeniuCojocaru@users.noreply.github.com>
2021-06-04 13:36:08 +0300

    App to update database

---
+++ UpdateDB/UpdateAnime_SenseiDB/UpdateAnime_SenseiDB/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace UpdateAnime_SenseiDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.genreList = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.add = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // genreList
+            // 
+            this.genreList.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genreList.FormattingEnabled = true;
+            this.genreList.Location = new System.Drawing.Point(2, 26);
+            this.genreList.Name = "genreList";
+            this.genreList.Size = new System.Drawing.Size(213, 688);
+            this.genreList.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(363, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(363, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Year";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(363, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Score";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(363, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Episodes";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(470, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(350, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(470, 125);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(470, 176);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(470, 223);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 4;
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(234, 308);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(592, 45);
+            this.add.TabIndex = 15;
+            this.add.Text = "ADD";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(234, 360);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(592, 354);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 720);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.genreList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox genreList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ UpdateDB/UpdateAnime_SenseiDB/UpdateAnime_SenseiDB/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UpdateAnime_SenseiDB
+{
+    public partial class Form1 : Form
+    {
+        static SqlConnection connection;
+        static string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\EugeniuCojocaru\\Desktop\\Limbaje formale si translatoare\\tema2\\DB\\anime.mdf\";Integrated Security=True;Connect Timeout=30";
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Genre", connection))
+            {
+                connection.Open();
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+
+                genreList.DataSource = null;
+                genreList.DataSource = table;
+                genreList.DisplayMember = "Genre";
+                genreList.CheckOnClick = true;
+
+                
+            }
+        }
+
+        void refresh_data_grid_view()
+        {            
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Anime", connection))
+            {
+                connection.Open();
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+
+                dataGridView1.DataSource = table;
+            }
+        }
+        private void add_Click(object sender, EventArgs e)
+        {
+            string title = textBox1.Text;
+            string year = textBox2.Text;
+            string score = textBox3.Text;
+            string episodes = textBox4.Text;
+            if (!title.Equals("") && !year.Equals("") && !score.Equals("") && !episodes.Equals(""))
+            {
+                string query_validate = "SELECT COUNT(*) as nb FROM Anime WHERE Title = '" + title + "'";
+                string query_get_id = "SELECT Id FROM Anime WHERE Title = '" + title + "'";
+                string query_insert_anime = "INSERT INTO Anime (Title,Year,Score,Episodes) VALUES ('" + title + "','" + year + "','" + score + "','" + episodes + "')";
+                string query_insert_anime_genre = "INSERT INTO Anime_Genre (Id_anime,Id_genre) VALUES ('";
+                using (connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query_validate, connection);
+                    var reader = command.ExecuteReader();
+                    if (reader.Read())
+                        if (int.Parse(reader["nb"].ToString().Trim()) != 0)
+                        {
+                            MessageBox.Show("Anime already in db!");
+                            return;
+                        }
+                        else
+                        {
+                            reader.Close();
+                            command = new SqlCommand(query_insert_anime, connection);
+                            command.ExecuteNonQuery();
+                            command = new SqlCommand(query_get_id, connection);
+                            reader = command.ExecuteReader();
+                            if (reader.Read())
+                            {
+                                string id = reader["Id"].ToString().Trim();
+                                if (genreList.SelectedItems.Count != 0)
+                                {
+                                    reader.Close();
+                                    for (int i = 0; i < genreList.Items.Count; i++)
+                                    {
+                                        if (genreList.GetItemChecked(i))
+                                        {
+                                            string q = query_insert_anime_genre + id + "','" + (i+1).ToString() + "')";
+                                            Console.WriteLine(q);
+                                            command = new SqlCommand(q, connection);
+                                            command.ExecuteNonQuery();
+
+                                        }
+                                    }
+                                    MessageBox.Show("Anime " + title + " added successfully");
+                                    refresh_data_grid_view();
+                                }
+                                else
+                                    MessageBox.Show("Select genres from list");
+                            }
+                            else
+                                MessageBox.Show("Can't find anime Id");
+                        }
+
+
+                }
+            }
+        }
+    }
+}
